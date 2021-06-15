3ca7c67c8f9668ef6cdcf855b39e93aa936b834e
BenLarry <4muki4@gmail.com>
2021-06-15 20:13:34 +0300

    css folder and file, images folder for icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,27 @@
 
 let weaponArr = ['rock', 'paper', 'scissors'];
-const playerSelection = 'rock';
+const playerSelection = playerPlay();
 const computerSelection = computerPlay();
 
 function computerPlay() {
     return weaponArr[Math.floor(Math.random() * weaponArr.length)];
 }
 
+function playerPlay() {
+    let weaponChoice = prompt("Choose: rock, paper or scissors");
+    for(const i of weaponArr) {
+        if(weaponChoice.toLowerCase() === i) {
+            return weaponChoice;
+        }
+    }
+    return console.log("not match")
+}
+
 function playRound(playerSelection, computerSelection) {
     if(playerSelection !== computerSelection) {
         return `NOT TIE player: ${playerSelection}, computer: ${computerSelection}`;
-    }else {
-        return `TIE player: ${playerSelection}, computer: ${computerSelection}`;
-    }       
+    }
+    return `TIE player: ${playerSelection}, computer: ${computerSelection}`;      
 }
 
 function game() {
