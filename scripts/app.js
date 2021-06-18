f7880aa650d258fc421b3d81a0aa65088df4644b
BenLarry <4muki4@gmail.com>
2021-06-18 17:38:03 +0300

    Main logic completed and console version working

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,18 @@
 
-let weaponArr = ['rock', 'paper', 'scissors'];
+const weaponArr = ['rock', 'scissors', 'paper'];
 const playerSelection = playerPlay();
 const computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     return weaponArr[Math.floor(Math.random() * weaponArr.length)];
 }
 
 function playerPlay() {
-    let weaponChoice = prompt("Choose: rock, paper or scissors");
+    let weaponChoice = prompt("Choose: rock, paper or scissors").toLowerCase();
     for(const i of weaponArr) {
-        if(weaponChoice.toLowerCase() === i) {
+        if(weaponChoice === i) {
             return weaponChoice;
         }
     }
@@ -19,6 +21,26 @@ function playerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection !== computerSelection) {
+        switch(playerSelection) {
+            case 'rock':
+                if(computerSelection === weaponArr[1]) {
+                    return `WIN player: ${playerSelection}, computer: ${computerSelection}`;
+                }else {
+                    return `LOSE player: ${playerSelection}, computer: ${computerSelection}`; 
+                }
+            case 'paper':
+                if(computerSelection === weaponArr[0]) {
+                    return `WIN player: ${playerSelection}, computer: ${computerSelection}`;
+                }else {
+                    return `LOSE player: ${playerSelection}, computer: ${computerSelection}`; 
+                }
+            case 'scissors':
+                if(computerSelection === weaponArr[2]) {
+                    return `WIN player: ${playerSelection}, computer: ${computerSelection}`;
+                }else {
+                    return `LOSE player: ${playerSelection}, computer: ${computerSelection}`; 
+                }
+        }
         return `NOT TIE player: ${playerSelection}, computer: ${computerSelection}`;
     }
     return `TIE player: ${playerSelection}, computer: ${computerSelection}`;      
@@ -26,7 +48,7 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     for(let i = 0; i < 5; i++) {
-        console.log(i + playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerSelection));
     }
 }
 
